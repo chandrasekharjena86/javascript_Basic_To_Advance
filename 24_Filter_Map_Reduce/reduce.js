1b36aa2b9f2e0facb125b3d3e6b4941c89ce4b58
chandra sekhar <sekhsrchandra86@gmail.com>
2024-09-26 19:17:01 +0530

    code update

--- 24_Filter_Map_Reduce/reduce.js
+++ 24_Filter_Map_Reduce/reduce.js
@@ -1,1 +1,22 @@
+const myNums = [1, 2, 3];
+// const myTotal = myNums.reduce(function(accumlator,currentvalue){
 
+//     console.log(`accum ${accumlator} curval ${currentvalue}`);
+
+//     return accumlator+currentvalue
+// },0)
+
+const myTotal = myNums.reduce((accum, cur) => accum + cur, 0);
+console.log(myTotal);
+
+const shopingCart = [
+  { itemName: "js course", price: 2999 },
+  { itemName: "datascience course", price: 12999 },
+  { itemName: "Python", price: 699 },
+  { itemName: "react", price: 99 },
+  { itemName: "node", price: 29 },
+  { itemName: "Express", price: 1000 },
+];
+
+const priceToPay=shopingCart.reduce((accum, item) => accum + item.price, 0);
+console.log(priceToPay);
